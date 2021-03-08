1249aac6dceeb6d25d7f136f2680dabaced0467f
시몬즈 <78244639+simonj-entry@users.noreply.github.com>
2021-03-08 15:32:19 +0900

    Delete Like Sponsor directory

--- Like Sponsor/index.js
+++
@@ -1,8 +0,0 @@
-import {l, url, e, pj, ds, my, users} from "https://raw.githubusercontent.com/ddtt786GH/eapi/main/code/index.js"
-
-pj('5f606f5c90bc8c1726a9ee3a').like()
-pj('5f606f5c90bc8c1726a9ee3a').star()
-let yes = prompt('좋&관 후원 하신 작품으로 이동하시겠습니까?(갈려면 '네' 안갈려면 아무거나 입력)')
-if (yes == 네) {
-  open('https://playentry.org/simonj/5f606f5c90bc8c1726a9ee3a')
-}
