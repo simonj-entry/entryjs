c1e0281290d5cb418c87e5b695e6c55367d07034
시몬즈 <78244639+simonj-entry@users.noreply.github.com>
2021-03-09 18:49:32 +0900

    Update index.js

--- promotion/index.js
+++ promotion/index.js
@@ -1,14 +1,8 @@
 function start() {
 	$.ajax({url:"/api/discuss/",type:"POST",dataType:"json",data:{content:promotion,title:"홍보홍보!",groupNotice:false,images:[],category:"free"}});
 }
-var macrotime = 0;
-var setmacrotime = 0;
-let promotion = prompt("작품링크를 입력해주세요");
-promotiontime = prompt("홍보 사용 시간 주기 입력(분)");
-setpromotiontime = promotiontime * 60000;
-if (setpromotiontime > 1){
-	alert('홍보를 시작합니다.');
-	alert('simonj의 홍보프로그램을 사용해주셔서 감사합니다.');
-	start()
-	setInterval(start, setpromotiontme);
-}
+alert('악용하지 마세요');
+let url = prompt("작품링크를 입력해주세요");
+var time = 0;
+var settime = 0;
+time = prompt("홍보를 돌릴 주기를 입력해주세요(분)");
