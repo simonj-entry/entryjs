485b9f647fad2a81b1951c38c32f7c4730c607d0
시몬즈 <78244639+simonj-entry@users.noreply.github.com>
2021-03-10 13:21:14 +0900

    Update index.js

--- Macro/index.js
+++ Macro/index.js
@@ -1,5 +1,6 @@
 alert('악용하지마세요');
-var time = prompt("메크로를 돌릴 시간을 입력해주세요(단위=밀리초(1초=1000밀리초))");
+var time = prompt("메크로를 돌릴 시간을 입력해주세요(초)");
+var settime = time * 1000;
 alert('simonj의 메크로 프로그램을 사용해주셔서 감사합니다.');
 function start() {
 $('.editDescriptionBtn').trigger('click')
@@ -7,4 +8,4 @@ $('.editConfirmBtn').trigger('click')
 console.log('메크로 성공!')
 }
 start()
-setInterval(start, (time))
+setInterval(start, (settime))
